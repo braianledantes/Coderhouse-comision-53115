76b969093e48ad378a2efd1c003384ed47c59998
Braian Ledantes <braianledantes@outlook.com>
2024-03-01 16:55:44 -0300

    ruta de carrito lista

---
+++ routers/carts.router.js
@@ -0,0 +1,58 @@
+const { Router } = require('express')
+const CartsManager = require('../datamanagers/CartsManager.js')
+const ProductManager = require('../datamanagers/ProductManager.js')
+
+const cm = new CartsManager('./assets/corritos.json')
+const pm = new ProductManager('./assets/productos.json');
+
+(async () => {
+    await Promise.all([cm.initialize(), pm.initialize()])
+})()
+
+const router = Router()
+
+router.post('/', async (_, res) => {
+    try {
+        const newCart = await cm.addCart({ products: [] })
+        res.status(201).json(newCart)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+})
+
+router.get('/:cid', async (req, res) => {
+    const cid = req.params.cid
+
+    try {
+        const cart = await cm.getCartById(cid)
+        res.json({ products: cart.products })
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+})
+
+router.post('/:cid/product/:pid', async (req, res) => {
+    const { cid, pid } = req.params
+
+    try {
+        const cart = await cm.getCartById(cid)
+        // verifica que exista el producto, si no existe lanza un error
+        await pm.getProductById(pid)
+
+        const productCart = cart.products.find(e => e.product == pid)
+        if (productCart) {
+            productCart.quantity++
+        } else {
+            const newProductCart = { product: parseInt(pid), quantity: 1 }
+            cart.products.push(newProductCart)
+        }
+
+        const updatedCart = await cm.updateCart(cid, cart)
+
+        res.json(updatedCart)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+})
+
+module.exports = router
