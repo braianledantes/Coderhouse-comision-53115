2216fbcbd338cfd5dc54ae13a875a5ee7d338fe3
Braian Ledantes <braianledantes@gmail.com>
2024-05-29 09:36:35 -0300

    feat: Add ticket schema for MongoDB

---
+++ models/mongodb/schemas/ticket.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const ticketSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    purchase_datetime: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    purchaser: {
+        type: String,
+        required: true
+    }
+})
+
+const Ticket = mongoose.model('Ticket', ticketSchema, 'tickets');
+
+module.exports = Ticket
