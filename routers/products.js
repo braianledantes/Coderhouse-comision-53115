c2c76840708fa3446cbae67ce38ca1e6988f9057
Braian Ledantes <braianledantes@gmail.com>
2024-05-28 22:48:23 -0300

    refactor: Update products router to require admin privileges for creating, updating, and deleting products

--- routers/products.js
+++ routers/products.js
@@ -1,15 +1,16 @@
 const { Router } = require('express')
 const { validateGetProducts, validateNewProduct, validateUpdateProduct } = require('../middlewares/validations/products.validations')
+const { isUserAdmin } = require('../middlewares/auth')
 
 const createProductsRouter = ({ productsController }) => {
     const router = Router()
 
     router.get('/', validateGetProducts, productsController.getPaginationProducts)
-    router.post('/', validateNewProduct, productsController.createProduct)
+    router.post('/', isUserAdmin, validateNewProduct, productsController.createProduct)
     
     router.get('/:pid', productsController.getProduct)
-    router.put('/:pid', validateUpdateProduct, productsController.updateProduct)
-    router.delete('/:pid', productsController.deleteProduct)
+    router.put('/:pid', isUserAdmin, validateUpdateProduct, productsController.updateProduct)
+    router.delete('/:pid', isUserAdmin, productsController.deleteProduct)
 
     return router
 }
