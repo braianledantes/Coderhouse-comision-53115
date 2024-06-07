afe435163e229cc93389fca81ad9dcfd94433bd1
Braian Ledantes <braianledantes@gmail.com>
2024-06-07 16:16:31 -0300

    Update sessions router to use authentication middleware

--- routers/sessions.js
+++ routers/sessions.js
@@ -1,25 +1,25 @@
 const { Router } = require('express')
-
+const { userIsLoggedIn, userIsNotLoggedIn } = require('../middlewares/auth')
 const { validateUser, validateEmailAndPasswordUser } = require('../middlewares/validations/user.validations')
 
 const createSessionsRouter = ({ sessionsController }) => {
     const router = Router()
 
-    router.post('/login', validateEmailAndPasswordUser, sessionsController.login)
-    
-    router.get('/faillogin', sessionsController.faillogin)
-    
-    router.get('/logout', sessionsController.logout)
-    
-    router.post('/register', validateUser, sessionsController.register)
-    
-    router.get('/failregister', sessionsController.failRegister)
-    
-    router.get('/github', sessionsController.authenticateWithGithub)
-    
-    router.get('/githubcallback', sessionsController.githubCallback)
-    
-    router.get('/current', sessionsController.getCurrentUser)
+    router.post('/login', userIsNotLoggedIn, validateEmailAndPasswordUser, sessionsController.login)
+
+    router.get('/faillogin', userIsNotLoggedIn, sessionsController.faillogin)
+
+    router.get('/logout', userIsLoggedIn, sessionsController.logout)
+
+    router.post('/register', userIsNotLoggedIn, validateUser, sessionsController.register)
+
+    router.get('/failregister', userIsNotLoggedIn, sessionsController.failRegister)
+
+    router.get('/github', userIsNotLoggedIn, sessionsController.authenticateWithGithub)
+
+    router.get('/githubcallback', userIsNotLoggedIn, sessionsController.githubCallback)
+
+    router.get('/current', userIsLoggedIn, sessionsController.getCurrentUser)
 
     return router
 }
