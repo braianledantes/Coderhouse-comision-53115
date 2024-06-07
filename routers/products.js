cf3014143348e488ba7ee269c133e9a4d3e5a8d6
Braian Ledantes <braianledantes@gmail.com>
2024-06-07 17:44:45 -0300

    chore: Update products router to use userIsLoggedIn middleware for authentication

--- routers/products.js
+++ routers/products.js
@@ -1,16 +1,16 @@
 const { Router } = require('express')
 const { validateGetProducts, validateNewProduct, validateUpdateProduct } = require('../middlewares/validations/products.validations')
-const { isUserAdmin } = require('../middlewares/auth')
+const { isUserAdmin, userIsLoggedIn } = require('../middlewares/auth')
 
 const createProductsRouter = ({ productsController }) => {
     const router = Router()
 
     router.get('/', validateGetProducts, productsController.getPaginationProducts)
-    router.post('/', isUserAdmin, validateNewProduct, productsController.createProduct)
-    
+    router.post('/', userIsLoggedIn, isUserAdmin, validateNewProduct, productsController.createProduct)
+
     router.get('/:pid', productsController.getProduct)
-    router.put('/:pid', isUserAdmin, validateUpdateProduct, productsController.updateProduct)
-    router.delete('/:pid', isUserAdmin, productsController.deleteProduct)
+    router.put('/:pid', userIsLoggedIn, isUserAdmin, validateUpdateProduct, productsController.updateProduct)
+    router.delete('/:pid', userIsLoggedIn, isUserAdmin, productsController.deleteProduct)
 
     return router
 }
